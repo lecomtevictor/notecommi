481cacd40959e7d5edd4f3393cc66da5ea9cbb58
Victor Lecomte <lecomtevictor7@gmail.com>
2023-05-04 15:32:28 +0200

    version prof pas trop pt pour linstant

--- src/App.js
+++ src/App.js
@@ -1,45 +1,16 @@
-import React, { useEffect, useState } from 'react';
-import { Link } from 'react-router-dom';
-import './App.css';
+import React, { useEffect, useState } from "react";
+import { Link, Route, Routes } from "react-router-dom";
+
+import "./App.css";
+import Note from "./components/Note/Note";
 
 function App() {
-  const [notes, setNotes] = useState([]);
-  const [selectedNote, setSelectedNote] = useState(null);
-  const [newNoteTitle, setNewNoteTitle] = useState("");
-  const [newNoteContent, setNewNoteContent] = useState("");
+  const [notes, setNotes] = useState(null);
 
   const fetchNotes = async () => {
     const response = await fetch("/notes");
     const result = await response.json();
     setNotes(result);
-  };
-
-  const handleNoteClick = (note) => {
-    setSelectedNote(note);
-  };
-
-  const handleNewNoteSubmit = async (event) => {
-    event.preventDefault();
-
-    const response = await fetch("/notes", {
-      method: "POST",
-      headers: {
-        "Content-Type": "application/json"
-      },
-      body: JSON.stringify({ title: newNoteTitle, content: newNoteContent })
-    });
-
-    if (response.ok) {
-      const result = await response.json();
-      setNotes([...notes, result]);
-      setSelectedNote(result);
-      setNewNoteTitle("");
-      setNewNoteContent("");
-    }
-  };
-
-  const handleGoBack = () => {
-    setSelectedNote(null);
   };
 
   useEffect(() => {
@@ -49,46 +20,20 @@ function App() {
   return (
     <>
       <aside className="Side">
-        {selectedNote ? (
-          <div>
-            <h3>{selectedNote.title}</h3>
-            <p>{selectedNote.content}</p>
-            <button type="button" onClick={handleGoBack}>Retour</button>
-          </div>
-        ) : (
-          <div>
-            <form onSubmit={handleNewNoteSubmit}>
-              <label>
-                Titre:
-                <input type="text" value={newNoteTitle} onChange={(event) => setNewNoteTitle(event.target.value)} />
-              </label>
-              <label>
-                Contenu:
-                <textarea value={newNoteContent} onChange={(event) => setNewNoteContent(event.target.value)}></textarea>
-              </label>
-              <div>
-                <button type="submit">Créer une note</button>
-              </div>
-            </form>
-            <hr />
-            {Array.isArray(notes) && notes.map((note) => (
-              <div key={note.id} className='titre_notes'>
-                <button><Link to={`/notes/${note.id}`} onClick={() => handleNoteClick(note)}>{note.title}</Link></button>
-              </div>
-            ))}
-          </div>
-        )}
+        {notes &&
+          notes.map((note) => (
+            <Link to={`/notes/${note.id}`} className="Note-link">
+              {note.title}
+            </Link>
+          ))}
       </aside>
       <main className="Main">
-        {selectedNote && (
-          <div>
-            <h2>{selectedNote.title}</h2>
-            <p>{selectedNote.content}</p>
-          </div>
-        )}
+        <Routes>
+          <Route path="/notes/:id" Component={Note} />
+        </Routes>
       </main>
     </>
   );
-};
+}
 
 export default App;
--- src/index.js
+++ src/index.js
@@ -1,19 +1,18 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import { BrowserRouter } from 'react-router-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import App from "./App";
+import reportWebVitals from "./reportWebVitals";
+import { BrowserRouter } from "react-router-dom";
 
-ReactDOM.render(
-  <BrowserRouter> {/* Ajouter BrowserRouter */}
-    <React.StrictMode>
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(
+  <React.StrictMode>
+    <BrowserRouter>
       <App />
-    </React.StrictMode>
-  </BrowserRouter>,
-  document.getElementById('root')
+    </BrowserRouter>
+  </React.StrictMode>
 );
-
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))

60f67087b8ad5268bfaef0c6d98b5b50e0e41635
Victor Lecomte <lecomtevictor7@gmail.com>
2023-05-04 12:17:43 +0200

    ajout react-router-dom et des link dans le app.js

--- src/App.js
+++ src/App.js
@@ -1,26 +1,44 @@
 import React, { useEffect, useState } from 'react';
+import { Link } from 'react-router-dom';
 import './App.css';
 
 function App() {
-const {notes, setNotes} = useState([null]);
+  const [notes, setNotes] = useState([]);
+  const [selectedNote, setSelectedNote] = useState(null);
+
   const fetchNotes = async () => {
-    const response = await fetch("/posts");
+    const response = await fetch("/notes");
     const result = await response.json();
     setNotes(result);
   };
+
+  const handleNoteClick = (note) => {
+    setSelectedNote(note);
+  };
+
   useEffect(() => {
     fetchNotes();
   }, []);
-  console.log(notes);
-  
+
   return (
     <>
       <aside className="Side">
-        {notes && notes.map((note) => <span>{note.title}</span>)}
+        {Array.isArray(notes) && notes.map((note) => (
+          <div key={note.id} className='titre_notes'>
+            <button><Link to={`/notes/${note.id}`} onClick={() => handleNoteClick(note)}>{note.title}</Link></button>
+          </div>
+        ))}
       </aside>
-      <main className="Main"></main>
+      <main className="Main">
+        {selectedNote && (
+          <div>
+            <h2>{selectedNote.title}</h2>
+            <p>{selectedNote.content}</p>
+          </div>
+        )}
+      </main>
     </>
-
   );
 };
+
 export default App;

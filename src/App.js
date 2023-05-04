8bea5ee940f62e3af1611fc7b105373f65fd9654
Victor Lecomte <lecomtevictor7@gmail.com>
2023-05-04 12:31:04 +0200

    ajout de la création de notes dans le app.js

--- src/App.js
+++ src/App.js
@@ -5,6 +5,8 @@ import './App.css';
 function App() {
   const [notes, setNotes] = useState([]);
   const [selectedNote, setSelectedNote] = useState(null);
+  const [newNoteTitle, setNewNoteTitle] = useState("");
+  const [newNoteContent, setNewNoteContent] = useState("");
 
   const fetchNotes = async () => {
     const response = await fetch("/notes");
@@ -16,6 +18,30 @@ function App() {
     setSelectedNote(note);
   };
 
+  const handleNewNoteSubmit = async (event) => {
+    event.preventDefault();
+
+    const response = await fetch("/notes", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json"
+      },
+      body: JSON.stringify({ title: newNoteTitle, content: newNoteContent })
+    });
+
+    if (response.ok) {
+      const result = await response.json();
+      setNotes([...notes, result]);
+      setSelectedNote(result);
+      setNewNoteTitle("");
+      setNewNoteContent("");
+    }
+  };
+
+  const handleGoBack = () => {
+    setSelectedNote(null);
+  };
+
   useEffect(() => {
     fetchNotes();
   }, []);
@@ -23,11 +49,35 @@ function App() {
   return (
     <>
       <aside className="Side">
-        {Array.isArray(notes) && notes.map((note) => (
-          <div key={note.id} className='titre_notes'>
-            <button><Link to={`/notes/${note.id}`} onClick={() => handleNoteClick(note)}>{note.title}</Link></button>
+        {selectedNote ? (
+          <div>
+            <h3>{selectedNote.title}</h3>
+            <p>{selectedNote.content}</p>
+            <button type="button" onClick={handleGoBack}>Retour</button>
           </div>
-        ))}
+        ) : (
+          <div>
+            <form onSubmit={handleNewNoteSubmit}>
+              <label>
+                Titre:
+                <input type="text" value={newNoteTitle} onChange={(event) => setNewNoteTitle(event.target.value)} />
+              </label>
+              <label>
+                Contenu:
+                <textarea value={newNoteContent} onChange={(event) => setNewNoteContent(event.target.value)}></textarea>
+              </label>
+              <div>
+                <button type="submit">Créer une note</button>
+              </div>
+            </form>
+            <hr />
+            {Array.isArray(notes) && notes.map((note) => (
+              <div key={note.id} className='titre_notes'>
+                <button><Link to={`/notes/${note.id}`} onClick={() => handleNoteClick(note)}>{note.title}</Link></button>
+              </div>
+            ))}
+          </div>
+        )}
       </aside>
       <main className="Main">
         {selectedNote && (
